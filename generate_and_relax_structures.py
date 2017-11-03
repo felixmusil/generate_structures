d6564f57cf218181d1de9be33325a46928236c66
musil <felix.musil@epfl.ch>
2017-11-03 18:40:50 +0100

    1st generation step is more reliable now

--- generate_and_relax_structures.py
+++ generate_and_relax_structures.py
@@ -1,12 +1,14 @@
 from Pool.mpi_pool import MPIPool
 import sys
-from libs.utils import unskewCell,get_standard_frame,stdchannel_to_null
+from libs.utils import unskewCell,get_standard_frame,s2hms
 from libs.input_structure import input2crystal
 from libs.LJ_pressure import LJ_vcrelax
 import cPickle as pck
 import numpy as np
 from libs.io import Frame_Dataset_h5
 from tqdm import tqdm
+from time import time
+
 
 def generate_crystal_step_1(sites_z, seed, vdw_ratio, isotropic_external_pressure=1e-2, symprec=1e-5):
     crystal, sg, wki = input2crystal(sites_z, seed, vdw_ratio)
@@ -35,14 +37,17 @@ if __name__ == '__main__':
 
     vdw_ratio = 1.5
     sites_z = [14]
-
+    print 'Starting '
+    start = time()
     strides = np.arange(1000)*1000
-    for st,nd in tqdm(zip(strides[:-1],strides[1:])):
+    for st,nd in zip(strides[:-1],strides[1:]):
 
         inputs = [{'sites_z':sites_z,'seed':seed,'vdw_ratio':vdw_ratio} for seed in range(st,nd)]
 
         crystals = pool.map(generate_crystal_step_1_wrapper,inputs,disable_pbar=True)
 
         fout.dump_frames(crystals,inputs)
+
+        print '{}\t {} {}'.format(s2hms(time()-start),st,np)
 
     pool.close()
