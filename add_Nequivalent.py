6a56cd25f771ef0ed68978bed7792a88999aeaa6
musil <felix.musil@epfl.ch>
2017-11-09 13:17:37 +0100

    1st generation step is more reliable now

--- add_Nequivalent.py
+++ add_Nequivalent.py
@@ -16,20 +16,20 @@ from libmatch.soap import get_Soaps
 def add_Nequivalent(fn):
     frame_reader = Frame_Dataset_h5(fn,mode='r+',disable_pbar=True)
 
-
     frame_names = frame_reader.names
 
     frame_reader.open(mode='r+')
-
+    lll = 0
     for frame_name in frame_names:
         ff = frame_reader.load_frame(frame_name)
         sym_data = spg.get_symmetry_dataset(ff, symprec=1e-5)
         Nequivalent_site = len(np.unique(sym_data['equivalent_atoms']))
         frame_reader.f.attrs['Nequivalent_site'] = Nequivalent_site
-
+        if Nequivalent_site == 1:
+            lll += 1
     frame_reader.close()
 
-
+    return lll,fn
 
 if __name__ == '__main__':
     pool = MPIPool()
@@ -47,6 +47,9 @@ if __name__ == '__main__':
 
     inputs = [fn for fn in fns]
 
-    pool.map(add_Nequivalent,inputs)
+    Ns = pool.map(add_Nequivalent,inputs)
+
+    for N in Ns:
+        print N
 
     pool.close()
