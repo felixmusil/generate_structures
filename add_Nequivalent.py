c1e7bbfab76134da2d6bfda2f8cae6d2ae6573ce
musil <felix.musil@epfl.ch>
2017-11-09 15:38:42 +0100

    1st generation step is more reliable now

--- add_Nequivalent.py
+++ add_Nequivalent.py
@@ -14,31 +14,31 @@ from libmatch.soap import get_Soaps
 
 
 def add_Nequivalent(fn):
-    frame_reader = Frame_Dataset_h5(fn,mode='r',swmr_mode=False,disable_pbar=False)
+    frame_reader = Frame_Dataset_h5(fn,mode='r',swmr_mode=False,disable_pbar=True)
 
     frame_names = frame_reader.names
 
-    # frame_reader.open(mode='r+')
+    frame_reader.open(mode='r+')
     lll = 0
     for frame_name in frame_names:
         ff = frame_reader.load_frame(frame_name)
         sym_data = spg.get_symmetry_dataset(ff, symprec=1e-5)
         Nequivalent_site = len(np.unique(sym_data['equivalent_atoms']))
-        # frame_reader.f[frame_name].attrs['Nequivalent_site'] = Nequivalent_site
+        frame_reader.f[frame_name].attrs['Nequivalent_site'] = Nequivalent_site
         if Nequivalent_site == 1:
             lll += 1
-    # frame_reader.close()
+    frame_reader.close()
 
     return lll,fn
 
 if __name__ == '__main__':
-    # pool = MPIPool()
-    #
-    # if not pool.is_master():
-    #
-    #     pool.wait()
-    #     # pool.wait(callbacks=[fout.close,executor.shutdown])
-    #     sys.exit(0)
+    pool = MPIPool()
+
+    if not pool.is_master():
+
+        pool.wait()
+        # pool.wait(callbacks=[fout.close,executor.shutdown])
+        sys.exit(0)
 
 
     dataPath = '/home/musil/workspace/qmat/structures/'
@@ -46,10 +46,10 @@ if __name__ == '__main__':
     fns = glob(dataPath + 'relaxed_structures_step1_*.h5')
     print len(fns)
     inputs = [fn for fn in fns]
-    Ns = map(add_Nequivalent,inputs)
-    # Ns = pool.map(add_Nequivalent,inputs)
+    # Ns = map(add_Nequivalent,inputs)
+    Ns = pool.map(add_Nequivalent,inputs)
 
     for N in Ns:
         print N
 
-    # pool.close()
+    pool.close()
